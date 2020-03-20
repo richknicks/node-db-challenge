7703c9f13ef3bb1b8e3986af222ac5b2980c78de
richknicks <rlynch@gmail.com>
2020-03-20 12:28:55 -0400

    create knex file, migrations .env, server

---
+++ database/migrations/20200320121725_projects.js
@@ -0,0 +1,55 @@
+
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('projects', tbl => {
+      tbl.increments();
+      tbl.string('name', 128)
+        .notNullable();
+      tbl.string('description', 128)
+        .notNullable();
+      tbl.boolean('status')
+        .notNullable()
+        .defaultTo('false');
+    })
+    .createTable('resources', tbl => {
+      tbl.increments();
+      tbl.string('name', 128)
+        .notNullable();
+      tbl.string('description', 128)   
+    })
+    .createTable('tasks', tbl => {
+      tbl.increments();
+      tbl.string('description', 128)
+        .notNullable();
+      tbl.string('notes', 128)
+      tbl.boolean('status')
+        .defaultTo()
+        .notNullable();
+      tbl.integer('projects_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('projects')
+    })
+    .createTable('projects-resources', tbl => {
+      tbl.primary(['projects_id', 'resources_id']);
+      tbl.integer('projects_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('projects');
+      tbl.integer('resources_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('resources');   
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+    .dropTableIfExists("project_resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects")
+};
